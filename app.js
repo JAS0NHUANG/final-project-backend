98729d51aefdb62a8508a6b6c58ade7eb8a1031f
jas0nhuang <jas0nhuang@tuta.io>
2021-03-04 13:00:55 +0000

    Refactor
    - CreateApp function to make unit testing possiable.
    - Create app.js dedicated to start the app.
    - Ceate start script for standardized "npm run start".

--- app.js
+++ app.js
@@ -6,23 +6,25 @@ const bodyParser = require('body-parser');
 const movieController = require('./controllers/movie');
 const emailController = require('./controllers/email');
 
-const app = express();
+const createApp = async () => {
+  const app = express();
 
-const port = 999;
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  app.use(cors());
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cors());
+  app.get('/movies-intheaters', movieController.getMovies);
+  app.get('/movies-thisweek', movieController.getMoviesThisweek);
+  app.get('/movie-genres', movieController.getMovieGenres);
 
-app.get('/movies-intheaters', movieController.getMovies);
-app.get('/movies-thisweek', movieController.getMoviesThisweek);
-app.get('/movie-genres', movieController.getMovieGenres);
+  app.post('/subscribe', emailController.subscribeEmail);
+  app.get('/unsubscribe', emailController.unSubscribeEmail);
 
-app.post('/subscribe', emailController.subscribeEmail);
-app.get('/unsubscribe', emailController.unSubscribeEmail);
+  app.options('*', cors());
 
-app.listen(port, () => {
-  console.log('Hello Movie!');
-});
+  return app;
+}
 
-app.options('*', cors());
+module.exports = {
+  createApp
+}
